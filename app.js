5158b440ce3a92465b5c7ff85a0f4bc9e6440e46
Juancmedina <kmilo.medina07@hotmail.com>
2023-10-15 16:04:47 -0500

    AVANCE

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const { mongoConn } = require('./databases/configuration')
 const dotenv = require('dotenv').config()
+const cors = require('cors')
 
 mongoConn()
 
 const app = express()
 
 app.use(express.json())
+app.use(cors({
+    origin:'*'
+}))
 
 const test = require('./routes/test')
 const generos = require('./routes/genero')
